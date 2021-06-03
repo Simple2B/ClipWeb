f149af3079c57e33e460d9fcb93b8633f9325cc1
tsyboom <tsyboom@gmail.com>
2021-06-03 15:56:03 +0000

    add title

--- app/setup.py
+++ app/setup.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 from starlette.responses import RedirectResponse
 from app.routers import router as visit_router
+from app.config import settings
 
 
 def create_app() -> FastAPI:
     """Create the application instance"""
-    app = FastAPI()
+    app = FastAPI(title=settings.SERVER_NAME)
     app.include_router(visit_router)
 
     @app.get("/")
